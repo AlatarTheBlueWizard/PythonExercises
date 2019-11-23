de857ebcc6d03b44ecd39ca32d0cbaa010f8fa33
Ryan Gale <46852767+AlatarTheBlueWizard@users.noreply.github.com>
2019-11-23 11:08:49 -0500

    fixed date

--- divisors.py
+++ divisors.py
@@ -3,7 +3,7 @@ A program that asks the user for a number and then
 prints out a list of all the divisors of that number. 
 
 Author: Ryan Gale
-Date: 10/23/2019
+Date: 11/23/2019
 """
 
 num = int(input("Enter a number: "))
